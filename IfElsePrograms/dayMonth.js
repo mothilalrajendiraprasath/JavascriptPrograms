a5e143516e60e5a72a1de1c0f8efc7bb88d60ec6
Mothilal Rajendira Prasath <78716398+mothilalrajendiraprasath@users.noreply.github.com>
2021-11-01 07:52:11 +0530

    Add files via upload

---
+++ IfElsePrograms/dayMonth.js
@@ -0,0 +1,19 @@
+const readline =require("readline");
+const r1=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+r1.question("Enter the day:",function(day){
+console.log(day);
+r1.question("Enter the month:",function(month){
+console.log(month);
+r1.close();
+
+if((day<=20&&month<=6)&&(day>=20&&month>=3)&&(day<=31)){
+    console.log("True")
+}
+    else {
+        console.log("True")
+}
+});
+});
